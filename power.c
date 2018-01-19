ef80a335a00d0f340d17807f3a9ecfc51adcf079
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-01-19 16:38:45 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int b,exp;
+ printf("Enter the base number");
+ scanf("%d,&b);
+ printf("Enter the exponent");
+ scanf("%d",exp);
+ while(exp=!0)
+ {
+  result*=b;
+  --exp;
+  }
+  printf("\n answer is...");
+  return 0;
+  }
+
