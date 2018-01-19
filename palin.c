25b28efbd4bda5841627781039e1aac4faa17288
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-01-19 16:25:42 +0530

    Create palin.c

---
+++ palin.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,rem,rev=0,t;
+printf("Enter the number");
+scanf("%d",&n);
+t=n;
+while(n!=0)
+{
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+if(t==rev)
+{
+printf("yes");
+}
+else
+{
+printf("No");
+}
+return 0;
+}
