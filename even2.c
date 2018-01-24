099b72d7d6d6ce145249216675c86bff4f6b15dd
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-01-24 16:24:44 +0530

    Create even2.c

---
+++ even2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,f,l;
+printf("Enter the 1st interval");
+scanf("%d",&n);
+printf("Enter the 2nd interval");
+scanf("%d",&l);
+for(i=f;i<=1;i++)
+{
+if(i%2==0)
+{
+printf("%d is an even number");
+}
+else
+{
+printf("%d is a odd number");
+}
+return 0;
+}
