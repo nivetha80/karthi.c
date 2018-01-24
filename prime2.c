f95cec79a96bba1b0284c0d50cbdb2aea2e3c2b8
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-01-24 16:40:06 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,f,n,l;
+printf("Enter the 1st interval");
+scanf("%d",&n);
+printf("Enter the 2nd interval");
+scanf("%d",&l);
+for(i=f;i<=1;i++)
+{
+if(i%5==1)
+{
+printf("%d is prime no");
+else
+printf("%d is not prime no");
+}
+return 0;
+}
