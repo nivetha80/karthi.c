7f8cae1649a05520e65b4ac9cc40f20894abc3a4
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-02-07 10:57:09 +0000

    Create count string.c

---
+++ count string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int i,count=0;
+  char a[100];
+  scanf("%[^\n]s",a);
+  for(i=0;a[i]!=NULL;i++)
+  {
+  	if(a[i]!=' ')
+  	{
+  		count++;
+  	}
+  	
+  }
+  printf("%d",count);
+  return 0;
+}
