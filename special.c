c88d1bb0fc2c6af8e0183ea933d3b869232fbfec
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-02-09 10:27:52 +0000

    Create special.c

---
+++ special.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=0;
+	char a[100];
+	gets(a);
+	for(i=0;a[i]!=NULL;i++)
+	{
+		if((a[i]>='0')&&(a[i]<='9'))
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
