fa0754ee896c5c6745a2b17daceeacef2f2c097a
nivetha80 <31503901+nivetha80@users.noreply.github.com>
2018-02-26 15:42:23 +0530

    Create mithra.c

---
+++ mithra.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,l,r;
+	scanf("%d",&n);
+	scanf("%d",&l);
+	scanf("%d",&r);
+	if((n>l)&&(n<r))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+		return 0;
+}
